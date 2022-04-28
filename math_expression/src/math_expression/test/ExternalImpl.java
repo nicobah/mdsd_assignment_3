d94db2d7a06b5de05d02fac628d33514f19862b6
nicobah <42782542+nicobah@users.noreply.github.com>
2022-04-28 19:26:28 +0200

    externalUpload
    externalUpload

--- math_expression/src/math_expression/test/ExternalImpl.java
+++ math_expression/src/math_expression/test/ExternalImpl.java
@@ -1,19 +1,18 @@
-package math_expression.test;
-
-import math_expression.*;
-
+package math_expression;
+import java.math.*;
 class ExternalImpl implements Test27.External, Test28.External, Test29.External, Test30.External, Test31.External, Test34.External {
 
 	public int pi() {
-		return 0;
+		return 3;
 	}
 
 	public int sqrt(int n) {
-		return 0;
+		return (int) Math.sqrt(n);
 	}
 	
 	public int pow(int n, int m) {
-		return 0;
+		return (int) Math.pow(n, m);
 	}
+
 	
 }
